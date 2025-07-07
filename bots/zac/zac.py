3ae19799dcf373642477d877f8be3ea703913267
Zac Morgan <mzachary568@gmail.com>
2025-07-07 14:00:51 +0000

    zac is now a car

--- bots/zac/zac.py
+++ bots/zac/zac.py
@@ -3,7 +3,7 @@ import random
 
 class Zac(RobotBase):
     def __init__(self, x, y):
-        super().__init__(x, y, "zac")
+        super().__init__(x, y, "Zac")
     
     def getMoveDirection(self):
         """Zac moves randomly, avoiding walls"""
